d12210d31005f97d3e0a5de9e2a3f18ab02336e4
rgpegasus <gabinruiz11@gmail.com>
2025-12-13 23:32:40 +0100

    Updated background color

--- game/core/engine.py
+++ game/core/engine.py
@@ -5,18 +5,14 @@ from game.manager.page import PageManager
 
 class Engine:
     def __init__(self):
-        self.app = Ursina(title="Power Rift",
-                          borderless=True,
-                          fullscreen=False) 
-
+        self.app = Ursina(title="Power Rift", borderless=True, fullscreen=False) 
         window.size = Variables.WINDOW_SIZE
         window.position = (0,0)
-        window.color = color.black
-        
+        window.color = color.gray
+
         PageManager.load("menu")
 
     def run(self):
         self.app.run()
-
 
 engine = Engine()
