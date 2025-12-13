96a26cae2f1bbbfcbeafbdf0de0a9fda25199234
rgpegasus <gabinruiz11@gmail.com>
2025-12-13 23:52:48 +0100

    Added new variables

--- game/core/variables.py
+++ game/core/variables.py
@@ -5,5 +5,7 @@ monitor = get_monitors()[0]
 
 class Variables:
     WINDOW_SIZE = (monitor.width, monitor.height -1)
-    PLAYER_SPEED = 5
+    PLAYER_SPEED = 6
+    GRAVITY = 20
+    JUMP_FORCE = 10
 
