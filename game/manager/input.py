98f5e3d76a9f6cc0a53f453c01995ead2298608b
rgpegasus <gabinruiz11@gmail.com>
2025-12-14 00:46:47 +0100

    Added single click option and gamepad input

--- game/manager/input.py
+++ game/manager/input.py
@@ -1,26 +1,88 @@
-# Permet de gérer toutes les actions liés aux touches 
-from ursina import mouse
+from ursina import mouse, held_keys
 import keyboard
 
 class InputManager:
-    def pressed(self, action):
-        list = {
-            "left" : "q",
-            "right" : "d",
-            "down" : "s",
-            "jump" : "space",
+    def __init__(self):
+        self.key = {
+            "left": "q",
+            "right": "d",
+            "get off": "s",
+            "crouch": "left ctrl",
+            "jump": "space",
             "attack": "left mouse",
+            "play": "p"
         }
 
-        key = list.get(action)
+        self.prev_state = {}
+        self.gamepad = {
+            "left": ["gamepad dpad left"],
+            "right": ["gamepad dpad right"],
+            "get off": ["gamepad dpad down"],
+            "jump": ["gamepad a"],
+            "attack": ["gamepad b"],
+            "play": ["gamepad start"]
+        }
+
+    def pressed(self, action):
+        key = self.key.get(action)
         if not key:
             return False
-
         if key == "left mouse":
             return mouse.left
         elif key == "right mouse":
             return mouse.right
+        elif keyboard.is_pressed(key):
+            return True
+
+        gamepad = self.gamepad.get(action, [])
+        for key in gamepad:
+            if held_keys.get(key):
+                return True
+
+        if action == "left":
+            return held_keys.get("gamepad left stick x", 0) < -0.5
+        elif action == "right":
+            return held_keys.get("gamepad left stick x", 0) > 0.5
+        elif action == "get off":
+            return held_keys.get("gamepad left stick y", 0) < -0.5
         
-        return keyboard.is_pressed(key)
+        return False
+    
+    def click(self, action):
+        key = self.key.get(action)
+        current = False
+        if not key:
+            return False
+        if key == "left mouse":
+            current = mouse.left
+        elif key == "right mouse":
+            current = mouse.right
+        elif keyboard.is_pressed(key):
+            current = True
+
+        gamepad = self.gamepad.get(action, [])
+        for key in gamepad:
+            if held_keys.get(key):
+                current = True
+        if not current:
+            if action == "left":
+                current = held_keys.get("gamepad left stick x", 0) < -0.5
+            elif action == "right":
+                current = held_keys.get("gamepad left stick x", 0) > 0.5
+            elif action == "get off":
+                current = held_keys.get("gamepad left stick y", 0) < -0.5
+
+        previous = self.prev_state.get(action, False)
+        if current:
+            if not previous:
+                clicked = True
+            else:
+                clicked = False
+        else:
+            clicked = False
+        self.prev_state[action] = current
+
+        return clicked
+
 
 inputManager = InputManager()
