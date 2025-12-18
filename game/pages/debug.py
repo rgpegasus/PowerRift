68eb900acc29242f973c13370c1da0a553405344
rgpegasus <gabinruiz11@gmail.com>
2025-12-18 09:52:49 +0100

    Added the new character to all maps + Added a background music

--- game/pages/debug.py
+++ game/pages/debug.py
@@ -1,11 +1,11 @@
 from ursina import *
-from game.entities.exemple import Exemple
+from game.entities.demon import Demon
 from game.manager.resource import resourceManager
 backgroundMap = resourceManager.picture("background/map/test")
 class Scene(Entity):
     def __init__(self):
         super().__init__()
-        self.player = Exemple(scale=(0.6, 0.75), position=(0, 10))
+        self.player = Demon( scale=(0.6, 0.75), position=(-2, 10))
         self.ground = Entity(z=-1, name="solid", collider='box', model='quad', color= '#5d5720', scale=(20, 2), position=(0, -1))
         self.plat_left = Entity(z=-1, name="solid", texture=backgroundMap, collider='box', model='quad', color='#2f2629', scale=(3, 100), position=(-3, 50))
         self.plat_right = Entity(z=-1, name="solid",  texture=backgroundMap, collider='box', model='quad', color='#2f2629', scale=(3, 100), position=(3, 50))
@@ -13,7 +13,9 @@ class Scene(Entity):
 
         camera.orthographic = True
         camera.fov = 10
-
+        self.mainMusic = resourceManager.music("main")
+        if self.mainMusic:
+            self.mainMusic.play()
     def update(self):
         if self.player.x > -5 and self.player.x < 5:
             camera.x = self.player.x
--- game/pages/test.py
+++ game/pages/test.py
@@ -1,11 +1,12 @@
 from ursina import *
-from game.entities.exemple import Exemple
+from game.entities.demon import Demon
 from game.manager.resource import resourceManager
 backgroundMap = resourceManager.picture("background/map/test")
+
 class Scene(Entity):
     def __init__(self):
         super().__init__()
-        self.player = Exemple(scale=(0.6, 0.75), position=(0, 10))
+        self.player = Demon( scale=(0.6, 0.75), position=(-2, 10))
         self.background = Entity(z=2, model='quad', texture=backgroundMap, scale= (30, 15), position=(0, 0))
         self.ground = Entity(z=-1, name="solid", collider='box', model='quad', color= "#2a2f26", scale=(8.25, 2.5), position=(0.25, -2.25))
         self.plat_left = Entity(z=-1, name="solid", collider='box', model='quad', color='#2f2629', scale=(1.5, 3), position=(-5.5, 1.5))
@@ -13,9 +14,13 @@ class Scene(Entity):
         self.plat_topm = Entity(z=1, name="platform", collider='box', model='quad', color="#636226", scale=(1.74, 0.15), position=(0, 1.75))
         self.plat_topl = Entity(z=-1, name="solid",collider='box', model='quad', color='#2f2629', scale=(2.75, 0.65), position=(-2.25, 1.5))
         self.plat_right = Entity(z=-1, name="solid", collider='box', model='quad', color="#2f2629", scale=(2, 1.5), position=(6, 3.5))
-
+        
         camera.orthographic = True
         camera.fov = 10
+
+        self.mainMusic = resourceManager.music("main")
+        if self.mainMusic:
+            self.mainMusic.play()
 
     def update(self):
         if self.player.x > -5 and self.player.x < 5:
@@ -26,4 +31,5 @@ class Scene(Entity):
             camera.y = 2.5
         if self.player.y <= -20 or self.player.y >= 20 or self.player.x <= -5 and self.player.x >= 5:
             self.player.position=(0, 10)
+            self.player.physics.velocity_y = 0
             self.player.velocity_y = 0
